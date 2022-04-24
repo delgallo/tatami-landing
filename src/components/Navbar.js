ad71bd5ae0e5cf8f3124e7788754d3d132efb49d
Fylls <55017307+Fylls@users.noreply.github.com>
2022-04-24 20:40:16 +0200

    added copy

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,20 @@
 import "../styling/nav.css"
 import "../styling/button.css"
-import { PopupButton } from '@typeform/embed-react'
+import { PopupButton } from "@typeform/embed-react"
 import logo from "../assets/logo.png"
 
 const Navbar = () => {
-    return (
-        <nav>
-            <div className="logo">
-                <img src={logo} alt="logo"/>
-                <div className="nav-title">
-                    Tatami
-                </div>
-            </div>
-            <PopupButton id="kKo6RYeF" className="button-85">
-                Apply
-            </PopupButton>
-        </nav>
-    )
+	return (
+		<nav>
+			<div className="logo">
+				<img src={logo} alt="logo" />
+				<div className="nav-title">Tatami</div>
+			</div>
+			<PopupButton id="kKo6RYeF" className="button-85">
+				Apply
+			</PopupButton>
+		</nav>
+	)
 }
 
 export default Navbar
