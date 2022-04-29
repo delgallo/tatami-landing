6e65448375d5fe111e81e8a317e89ec6756a34ce
Giovanni Del Gallo <34619893+delgallo@users.noreply.github.com>
2022-04-29 20:39:16 +0200

    gtag click apply

--- src/components/Apply.js
+++ src/components/Apply.js
@@ -3,15 +3,13 @@ import { PopupButton } from "@typeform/embed-react"
 const Apply = () => {
 
     const queryParams = new URLSearchParams(window.location.search)
-    console.log("####")
-    console.log(Math.floor(Math.random() * 1000000))
 
     return(
-        <>
+        <div onClick={() => window.gtag('event', 'click_apply')}>
         	<PopupButton id={`kKo6RYeF#referral=${queryParams.get('r')}&hash=${Math.floor(Math.random() * 1000000)}`} className="button-85">
 				Apply
 			</PopupButton>
-        </>
+        </div>
     )
 }
 
